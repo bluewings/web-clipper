b1aeeff434371dcd9b11fb2868fb68a03058d26e
Albert Cha <mygenie3@naver.com>
2014-05-13 23:38:14 +0900

    change module

--- public/javascripts/web-clipper-inject.js
+++ public/javascripts/web-clipper-inject.js
@@ -2,35 +2,200 @@
 
     'use strict';
 
-    var view = {};
+    var $, view, status, app;
 
-    var HTML = {
+    var HTML;
+
+    HTML = {
         CONTAINER: ' ' +
-            '<div style="position:fixed;top:10px;right:10px;z-index:1000;"> ' +
-            '<button class="btn-summarize" style="padding:10px;">clip</button> ' +
-            '<button class="btn-clip" style="padding:10px;">cut</button> ' +
-            '</div>'
+            '<div ng-controller="clipper-main" ng-class="\'web-clipper-widget\' + (data.showWidget ? \' show\' : \'\')"> ' +
+            '  <div class="control"> ' +
+            '    <button class="btn-summarize"><span class="glyphicon glyphicon-user"></span>clip</button> ' +
+            '    <button ng-click="func.toggleMarker()" ng-style="data.useMarker ? \'color:red\' : \'\'" class="btn-clip">cut</button> ' +
+            '    <input type="text" ng-model="data.test"> ' +
+            '    <pre>{{data | json}}</pre> ' +
+            '  </div> ' +
+            '  <div ng-show="data.showWidget && data.useMarker" class="pointer"> ' +
+            '  </div> ' +            
+            '</div> '
     };
 
-    function includeScript(src, callback) {
+    status = {
+        useMarker: false
+    };
 
-        var script = document.createElement('SCRIPT');
-        script.onload = function () {
+    function initNgModule() {
 
-            if (callback && typeof callback == 'function') {
-                callback();
-            }
-        };
-        script.type = 'text/javascript';
-        script.src = src;
-        document.body.appendChild(script);
+        app = angular.module('web-clipper-widget', []);
+
+        app.controller('clipper-main', function($scope, $element) {
+
+            $scope.data = {
+
+                showWidget: true,
+                useMarker: status.useMarker
+            };
+
+            $scope.$watch('data.useMarker', function(newValue, oldValue) {
+
+                status.useMarker = newValue;
+            });
+
+            $scope.func = {
+                toggleMarker: function() {
+
+                    $scope.data.useMarker = $scope.data.useMarker ? false : true;
+                }
+            };
+
+            $(document.body).on('dblclick', function(event) {
+
+                $scope.$apply(function() {
+                $scope.data.showWidget = $scope.data.showWidget ? false : true;
+                });
+                event.preventDefault();
+            }).on('mousemove', function(event) {
+
+                $element.find('.pointer').css({
+                    top: event.pageY - 25,
+                    left: event.pageX
+                });
+            });
+        });
     }
+
 
 
 
     function initialize() {
 
+        initNgModule();
+
         initView();
+
+        console.log('initialize');
+
+        rangy.init();
+
+        // Enable multiple selections in IE
+        try {
+            document.execCommand("MultipleSelection", true, true);
+        } catch (ex) {
+            // noop
+        }
+
+        angular.bootstrap(view.container.get(0), ['web-clipper-widget']);
+
+
+        $(document.body).on('dblclick', function() {
+
+
+        });
+
+
+        $(document.body).on('mouseup', function () {
+
+
+            console.log('mouseup');
+            console.log(rangy);
+            if (!status.useMarker) {
+                return;
+
+            }
+
+            console.log(rangy.getSelection().getRangeAt(0).toString());
+            console.log(rangy.getSelection().getRangeAt(0));
+
+            console.log(rangy.getSelection().getRangeAt(0));
+
+            var sel = rangy.getSelection().getRangeAt(0);
+
+            console.log('>>> client rects');
+
+            console.log(sel.nativeRange.getClientRects());
+
+            var rectList = sel.nativeRange.getClientRects();
+            var div;
+
+            var scrollTop = $(window).scrollTop(),
+                scrollLeft = $(window).scrollLeft();
+
+            var rect;
+
+            for (var inx = 0; inx < rectList.length; inx++) {
+
+                rect = rectList[inx];
+
+                if (rect.width > 0 && rect.height) {
+                    div = $('<div></div>');
+                    $(document.body).append(div);
+                    div.css({
+                        position: 'absolute',
+                        zIndex: 20000,
+                        top: rect.top + scrollTop,
+                        left: rect.left + scrollLeft,
+                        width: rect.width,
+                        height: rect.height,
+                        backgroundColor: 'rgba(255,0,0,0.1)'
+
+                    });
+                }
+
+
+
+            }
+
+            console.log('>>> bounding rect');
+
+            console.log(sel.nativeRange.getBoundingClientRect());
+
+            var rect = sel.nativeRange.getBoundingClientRect()
+
+
+            if (rect.width > 0 && rect.height) {
+                div = $('<div></div>');
+                $(document.body).append(div);
+                div.css({
+                    position: 'absolute',
+                    zIndex: 20000,
+                    top: rect.top + scrollTop,
+                    left: rect.left + scrollLeft,
+                    width: rect.width,
+                    height: rect.height,
+                    backgroundColor: 'rgba(255,0,0,0.05)'
+
+                });
+            }
+
+
+            rangy.getSelection().collapseToEnd();
+            //alert('collapse');
+
+            //getClientRects
+
+            return;
+
+            console.log(rangy);
+
+            var range = rangy.createRange();
+
+
+
+            // All DOM Range methods and properties supported
+            range.selectNodeContents(document.body);
+
+            console.log(range);
+
+            //console.log(rangy.getSelection().getRangeAt(0));
+
+            return;
+
+            // Selection object based on those in Mozilla, WebKit and Opera
+            var sel = rangy.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+
+        });
 
         //attachEventHandler();
 
@@ -73,89 +238,89 @@
 
         view.button.clip.on('click', function () {
 
-        	var mask = $('<div class="clip_mask"></div>');
+            var mask = $('<div class="clip_mask"></div>');
 
-        	var canvas = document.createElement('canvas'),
-        		ctx = canvas.getContext('2d');
-
-
-
-        	$(document.body).append(mask);
-
-        	mask.css({
-
-        		position: 'absolute',
-        		zIndex: 10000,
-        		top:0,
-        		left: 0,
-        		width: '100%',
-        		height: '100%',
-        		backgroundColor: 'rgba(0,255,255,0.1)'
-
-        	});
-
-        	canvas.width = mask.outerWidth();
-        	canvas.height = mask.outerHeight();
-
-        	mask.append(canvas);
+            var canvas = document.createElement('canvas'),
+                ctx = canvas.getContext('2d');
 
 
 
-        	var onclipping = false;
+            $(document.body).append(mask);
 
-        	mask.on('mousedown', function(event) {
+            mask.css({
 
-        		if (onclipping === false) {
+                position: 'absolute',
+                zIndex: 10000,
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0,255,255,0.1)'
 
-        			ctx.moveTo(event.pageX, event.pageY);
-        			onclipping = true;
+            });
 
-        		}
+            canvas.width = mask.outerWidth();
+            canvas.height = mask.outerHeight();
 
-
-
-        	}).on('mousemove', function(event) {
-
-        		if (onclipping) {
-
-
-        			ctx.lineTo(event.pageX, event.pageY);
-        			ctx.stroke();
-
-        		}
+            mask.append(canvas);
 
 
 
-        	}).on('mouseup', function(event) {
+            var onclipping = false;
+
+            mask.on('mousedown', function (event) {
+
+                if (onclipping === false) {
+
+                    ctx.moveTo(event.pageX, event.pageY);
+                    onclipping = true;
+
+                }
 
 
 
-        		if (onclipping) {
-        			onclipping = false;
-        		}
+            }).on('mousemove', function (event) {
+
+                if (onclipping) {
+
+
+                    ctx.lineTo(event.pageX, event.pageY);
+                    ctx.stroke();
+
+                }
 
 
 
-        	});
+            }).on('mouseup', function (event) {
+
+
+
+                if (onclipping) {
+                    onclipping = false;
+                }
+
+
+
+            });
 
 
 
 
-        });        
+        });
 
 
 
         $(document.body).on('click', function (event) {
 
-        	return;
+            return;
 
-        	html2canvas(document.body, {
-					  onrendered: function(canvas) {
-					    document.body.appendChild(canvas);
-					  }
-					});
+            html2canvas(document.body, {
+                onrendered: function (canvas) {
+                    document.body.appendChild(canvas);
+                }
+            });
 
-        	return;
+            return;
 
             console.log(event);
             var div = $('<div>');
@@ -266,13 +431,6 @@
 
     }
 
-
-    function summarize() {
-
-        alert('summarize');
-
-    }
-
     function getAbsoluteBoundingRect(el) {
         var doc = document,
             win = window,
@@ -306,28 +464,81 @@
             width: rect.width
         };
     }
-    //return;
 
-    if (window.jQuery === undefined || window.html2canvas === undefined) {
-
-        includeScript('http://10.64.51.102:2000/components/jquery/dist/jquery.min.js', function () {
-
-            includeScript('http://10.64.51.102:2000/components/html2canvas/build/html2canvas.min.js', function () {
-
-                console.log(html2canvas);
-
-                initialize();
-
-            });
+    //    return;
 
 
+    //////////////////////////
 
 
+    function bootstrap() {
 
+        var resources = [],
+            noConflict = false;
+
+        if (window.jQuery === undefined) {
+            resources.push('http://10.64.51.102:2000/components/jquery/dist/jquery.min.js');
+        } else if (window.$ && window.$ !== window.jQuery) {
+            noConflict = true;
+        }
+        resources.push('http://10.64.51.102:2000/components/angular/angular.min.js');
+        resources.push('http://10.64.51.102:2000/components/html2canvas/build/html2canvas.min.js');
+        resources.push('http://10.64.51.102:2000/components/rangy-1.3/rangy-core.js');
+        resources.push('http://10.64.51.102:2000/stylesheets/web-clipper-inject.css');
+
+        loadResources(resources, function () {
+
+            if (noConflict) {
+                window.$.noConflict();
+            }
+            $ = window.jQuery;
+
+            initialize();
         });
-    } else {
-
-        initialize();
     }
+
+    function loadResources(srcList, callback) {
+
+        var inx, element, type, elements = [];
+
+        if (typeof srcList == 'string') {
+            srcList = [srcList];
+        }
+
+        for (inx = 0; inx < srcList.length; inx++) {
+            type = srcList[inx].match(/\.js$/) ? 'script' : 'css';
+            element = document.createElement(type == 'script' ? 'SCRIPT' : 'LINK');
+            elements.push(element);
+            element.onload = function () {
+                var inx, ready = true;
+                this.__ready = true;
+                for (inx = 0; inx < elements.length; inx++) {
+                    if (!elements[inx].__ready) {
+                        ready = false;
+                        break;
+                    }
+                }
+                if (ready && callback && typeof callback == 'function') {
+                    callback();
+                }
+            };
+            if (type == 'script') {
+                element.type = 'text/javascript';
+                element.src = srcList[inx];
+            } else {
+                element.rel = 'stylesheet';
+                element.type = 'text/css';
+                element.href = srcList[inx];
+            }
+
+            document.body.appendChild(element);
+        }
+    }
+
+    setTimeout(function () {
+
+        bootstrap();
+
+    }, 100);
 
 })();
